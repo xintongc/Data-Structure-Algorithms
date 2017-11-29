7ed89c9a67f8204dcae66cd9ce91a7b6b4443213
Claire (Xintong) <xintongc@gmail.com>
2017-11-29 15:30:39 -0500

    Update findTheMostOne.java

--- findTheMostOne.java
+++ findTheMostOne.java
@@ -2,8 +2,7 @@
 // and 0's such that, in any row of A, all the 1's come
 // before any of the 0's in that row. Assuming A is
 // already in memory, describe a method running in
-// O(n) time (not O(n2
-// ) time) for finding the row of A
+// O(n) time (not O(n2) time) for finding the row of A
 // that contains the most 1's.    
 
 
